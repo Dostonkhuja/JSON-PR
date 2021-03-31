79506324db926827b5acb61ea2540288a1288bfd
Dostonkhuja <sheraliyevdostonhoja@gamil.com>
2021-04-01 02:12:44 +0300

    deploy github pages

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import store from "./state-managment/redux-store";
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>, document.getElementById('root')
+    </HashRouter>, document.getElementById('root')
         );
 
         // If you want to start measuring performance in your app, pass a function
